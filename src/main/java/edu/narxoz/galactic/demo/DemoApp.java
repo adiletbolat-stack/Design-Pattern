37489696f25538b86b68624d6ee95e9ee300b1f3
adiletbolat <adilet.bolat@narxoz.kz>
2026-02-01 20:24:07 +0500

    Fifth. Removed imports which are never used

--- src/main/java/edu/narxoz/galactic/demo/DemoApp.java
+++ src/main/java/edu/narxoz/galactic/demo/DemoApp.java
@@ -4,11 +4,9 @@ import edu.narxoz.galactic.bodies.Planet;
 import edu.narxoz.galactic.cargo.Cargo;
 import edu.narxoz.galactic.dispatcher.Dispatcher;
 import edu.narxoz.galactic.dispatcher.Result;
-import edu.narxoz.galactic.drones.DroneStatus;
 import edu.narxoz.galactic.drones.HeavyDrone;
 import edu.narxoz.galactic.drones.LightDrone;
 import edu.narxoz.galactic.task.DeliveryTask;
-import edu.narxoz.galactic.task.TaskState;
 
 public class DemoApp {
 
